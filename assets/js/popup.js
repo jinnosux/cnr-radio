eebb520d1216f1f11936e505b22810b39ebdfdfc
jinnosux <vahid.k@ibu.edu.mk>
2022-11-09 20:20:58 +0100

    latest radio changes

---
+++ assets/js/popup.js
@@ -0,0 +1,18 @@
+const popup1 = document.getElementById('popup1');
+const popup2 = document.getElementById('popup2');
+
+show1 = function () {
+    popup1.style.display = 'block';
+}
+
+hide1 = function () {
+    popup1.style.display = 'none';
+}
+
+show2 = function () {
+    popup2.style.display = 'block';
+}
+
+hide2 = function () {
+    popup2.style.display = 'none';
+}
